6fb9bb99f5f37c444962ce8a935002d474c369cf
AnthonyA <alicea.9a@gmail.com>
2025-01-21 13:02:03 -0700

    feat: Add data to 01_seeds and insert into database tables

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,21 @@
+INSERT INTO users (name,email,password)
+VALUES('Franky Smith','frankster21@mail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('Robby Olderman','thatguy54@mail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u'),
+('Sarah Lin','linS@mail.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u');
+
+INSERT INTO properties (owner_id,title,description,thumbnail_photo_url,cover_photo_url,cost_per_night,parking_spaces,number_of_bathrooms,number_of_bedrooms,country,street,city,province,post_code,active)
+VALUES (1,'Hibbit Hole','description','https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg?auto=compress&cs=tinysrgb&h=350','https://images.pexels.com/photos/2086676/pexels-photo-2086676.jpeg', 96432,1,1,1,'Canada','0 Hole Valley','The Shimbles','Quebec','28142','true'),
+(2,'Shabby Cottage','description','https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2121121/pexels-photo-2121121.jpeg', 62443,3,3,4,'Canada','651 Nami Road','Bohbatev','Alberta', '45234','true'),
+(1,'Mansion Manor', 'description','https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=350', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg', 12345, 12, 15, 23, 'Canada', '1650 Hejto Center', 'Genwezuj', 'Newfoundland And Labrador', '34234', 'true');
+
+INSERT INTO reservations (start_date, end_date, property_id, guest_id)
+VALUES ('2018-09-11', '2018-09-26', 1, 3),
+('2019-01-04', '2019-02-01', 2, 1),
+('2021-10-01', '2021-10-14', 3, 2);
+
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
+VALUES(3, 1, 1, 5, 'messages'),
+(2, 1, 2, 4, 'messages'),
+(2, 3, 3, 1, 'messages');
+
+
